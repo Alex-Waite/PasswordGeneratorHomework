84c462cc238781f1617548ee966ef20cd95ddd9f
Alex Waite <alexanderconorwaite@gmail.com>
2020-03-05 23:30:39 +1030

    Functional Uppercase letter generator

--- script.js
+++ script.js
@@ -15,7 +15,12 @@ function getRandomLowercase() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
 
+function getRandomUppercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
 console.log(getRandomLowercase())
+console.log(getRandomUppercase())
 
 
 
